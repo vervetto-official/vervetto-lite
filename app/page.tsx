11e7abff1feddbc979afd7adee2ed833b7bdebe2
Saqib Hasib <saqibhasib@gmail.com>
2024-09-25 00:18:14 -0700

    fixed tailwind config file issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header1/>
-      {/* <h1 className="bg-black">this is Header1</h1> */}
-      <a href="#" className="bg-red">testing</a>
     </main>
   );
 }
--- blocks/header-1/index.tsx
+++ blocks/header-1/index.tsx
@@ -3,9 +3,8 @@ import { Button } from "@/components/button";
 export const Header1 = () => {
     return (
         <>
-            <h1 className="bg-black">this is Header1</h1>
-            <a href="#" className="bg-red-600">testing</a>
-            <Button type="link" href="#" className="bg-[#50d71e]">Get Started!</Button>
+            <h1 className="bg-black text-white p-3">this is Header1</h1>
+            <Button type="link" href="#" className="bg-[#50d71e] p-3">Get Started!</Button>
         </>
     );
 }
