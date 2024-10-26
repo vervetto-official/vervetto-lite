d8717ee3405eb9974b6a7af5f5a1b1967193e389
Aqib Ashef <aqib1952@gmail.com>
2024-10-26 16:56:02 +0600

    top navigation wip

--- blocks/navigation-top/index.tsx
+++ blocks/navigation-top/index.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/button';
 
 import styles from './navigation-top.module.css';
 
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 
 import { config } from '@/config';
 
@@ -16,11 +16,25 @@ interface NavigationTopProperties {
 
 export const NavigationTop = ({ className  = "" } : NavigationTopProperties) => {
     const [isOpen, setOpen] = useState(false);
+    const [isChildMenuOpen, setChildMenuOpen] = useState(new Map<String, boolean>());
     let navMenuItems = config.navigationMenu;
 
     function toggleOpen() {
         setOpen(!isOpen);
     }
+
+    function toggleChildMenuOpen(event: MouseEvent<HTMLAnchorElement>) {
+        // event.preventDefault();
+        console.log(event.currentTarget.id);
+        let id = event.currentTarget.id;
+        let tmpChildMenuOpenState = isChildMenuOpen;
+        tmpChildMenuOpenState.set(id, tmpChildMenuOpenState.has(id) ? !tmpChildMenuOpenState.get(id): true);
+        setChildMenuOpen(tmpChildMenuOpenState);
+        console.log(isChildMenuOpen);
+        console.log(isChildMenuOpen.get(id));
+        console.log(isChildMenuOpen.has(id));
+    }
+
     return (
         <nav className={ "flex justify-center " + styles.nav + " " + className}>
             <div className= {"container gap-8 py-5 px-5 lg:px-8 xl:px-0 flex flex-row justify-between items-center"}>
@@ -42,12 +56,19 @@ export const NavigationTop = ({ className  = "" } : NavigationTopProperties) => 
                                 }
                                 else {
                                     return (
-                                        <li className={styles.hasChild + " lg:hover:bg-black/10 py-5 lg:py-1.5 pl-0 lg:pl-4 pr-5 rounded-md border-b border-gray-200 lg:border-0 last:border-0"}>
-                                            <Link href={"#"}>{item.text}</Link>
-                                            <ul className={styles.childNav + " absolute min-w-max bg-white rounded-b-md shadow px-1.5 py-2.5"}>
+                                        <li 
+                                            key={index}
+                                            className={styles.hasChild + " " + ((isChildMenuOpen.has("childNav_" + index.toString()) && isChildMenuOpen.get("childNav_" + index.toString())) ? styles.expanded : "") + " flex-col lg:flex-row lg:hover:bg-black/10 py-5 lg:py-1.5 pl-0 lg:pl-4 pr-5 rounded-md border-b border-gray-200 lg:border-0 last:border-0"}
+                                        >
+                                            <Link
+                                                id={"childNav_" + index.toString()}
+                                                href={"#"}
+                                                onClick={toggleChildMenuOpen}
+                                            >{item.text}</Link>
+                                            <ul className={styles.childNav + " relative lg:absolute min-w-max bg-gray-100 lg:bg-white rounded-t-md lg:rounded-t-none rounded-b-md lg:shadow px-1.5 py-2.5"}>
                                                 {item.children.map((childItem, childIndex) => {
                                                     return (
-                                                        <li key={"child-item" + childIndex} className={ styles.navItem + " min-w-40 rounded-md py-2 px-4 hover:bg-gray-100"}>
+                                                        <li key={"child-item" + childIndex} className={ styles.navItem + " min-w-40 rounded-md py-2 px-4 hover:bg-gray-200"}>
                                                             <Link href={childItem.link} className="w-full flex">{childItem.text}</Link>
                                                         </li>
                                                     );
